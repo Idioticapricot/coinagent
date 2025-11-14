06733b4d39547ff64ef1a23ffaf68a63762096fd
nickthelegend <testingtesla7@gmail.com>
2025-11-15 01:52:49 +0530

    Edited New Agent ID

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ def handle_task(job_input: str) -> str:
 if __name__ == "__main__":
     config = AgentConfig(
         agent_id="5bbf48cf-62a2-4ab3-bd96-1d29ba0fc1f3",
-        receiver_address="QLPYRAB2R36W4KQL7PFVG2VJRO4YO5FUD7LRNHSV33LG42C2WELUTPSIZY",
+        receiver_address="PDQRQJL2TZYJQKKKBPYYXZX5J33QFEYWXUHQDKAHL63TFIGCHHXLNSWWSM",
         price_microalgos=1_000_000,
-        agent_token="8df55d2d525093fd8c3d357ba54300a55cab9c7f8fd24166583825bd5d9ed311",
+        agent_token="5486b0f706e4054400ca6e1e9060b686f09a8d8a4c9602d7969e43dee958b3a4",
         remote_server_url="http://localhost:3000/api/agent/access",
-        app_id=749656277,
+        app_id=749660669,
     )
 
     AgentServer(config=config, handler=handle_task).run()
